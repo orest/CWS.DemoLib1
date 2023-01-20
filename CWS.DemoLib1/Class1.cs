a35e39675809876e3ebe85132cef604cb5011337
Orest VM <orest.khrapko@cardworks.com>
2023-01-20 16:23:49 -0500

    Add project files.

---
+++ CWS.DemoLib1/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace CWS.DemoLib1
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+        public decimal CurrentBalance { get; set; }
+        public decimal CreditLimit { get; set; }
+        public decimal AvailableCredit { get; set; }
+
+
+
+    }
+}
